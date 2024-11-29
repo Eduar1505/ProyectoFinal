5e9fd167ecba476fc3062850f15044caf94d7a32
eventuraa15 <141580602+Eduar1505@users.noreply.github.com>
2024-11-29 17:43:39 -0600

    class button

--- eje_agenda/lib/componentes.dart/botones.dart
+++ eje_agenda/lib/componentes.dart/botones.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-  const MyButton({super.key});
+  final Function()? onTap;
+
+  const MyButton({super.key, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(25),
+        margin: const EdgeInsets.symmetric(horizontal: 25),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+        ),
+        child: const Center(
+          child: Text(
+            'CONTINUAR',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 40,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
