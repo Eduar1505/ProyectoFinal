77ebee7aea660221fff64733e7bbc154b9fad6c6
EmersonGalindo <emerson.galindo@unah.hn>
2024-11-29 16:59:38 -0600

    bienvenida

--- eje_agenda/lib/paginas/inicio.dart
+++ eje_agenda/lib/paginas/inicio.dart
@@ -5,6 +5,19 @@ class StartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return  Scaffold(
+      backgroundColor: Colors.indigo[900],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(Icons.event_note, size: 100, color: Colors.blue,),
+            const SizedBox(height: 20,),
+            const Text('eje agenda', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),),
+            const SizedBox(height: 50,),
+          ]
+        ),
+      ),
+    );
   }
 }
