4ff99d4201136d107f058e9616ebcaf4175c7c04
01molina <jdmolinar@unah.hn>
2024-11-29 15:34:58 -0600

    home page

--- eje_agenda/lib/paginas/home_page.dart
+++ eje_agenda/lib/paginas/home_page.dart
@@ -8,11 +8,52 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  
+  final List<String> _recordatorios = [];
+  final TextEditingController _textController = TextEditingController();
+
+  void _agregarRecordatorio(){
+    String texto = _textController.text;
+    if (texto.isNotEmpty){
+      setState((){
+        _recordatorios.add(texto);
+        _textController.clear();
+      });
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text('Recordatorios'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: conts EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _textController,
+              decoration: InputDecoraton(
+                labelText: 'Nuevo recordatorio',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: _agregarRecordatorio,
+            child: Text('Agregar'),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _recordatorios.length,
+              itemBuilder: (context, index){
+                return ListTile(
+                  title: Text(_recordatorios[index]),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
