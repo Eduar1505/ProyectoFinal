f4892a3dd70f084c1d77aa7c435ae1904200421e
eventuraa15 <141580602+Eduar1505@users.noreply.github.com>
2024-11-30 11:11:00 -0600

    apartado para agregar tareas o notas

---
+++ eje_agenda/lib/paginas/agregar_tareas.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AgregarTareas extends StatelessWidget {
+  const AgregarTareas({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
