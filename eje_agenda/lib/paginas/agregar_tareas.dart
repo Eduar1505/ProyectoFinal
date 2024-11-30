6dd629cfea3c798ec02b8b240e326fe180a40955
eventuraa15 <141580602+Eduar1505@users.noreply.github.com>
2024-11-30 12:43:15 -0600

    diseno de agregartareas sin funciones

--- eje_agenda/lib/paginas/agregar_tareas.dart
+++ eje_agenda/lib/paginas/agregar_tareas.dart
@@ -39,6 +39,34 @@ class _AgregarTareasState extends State<AgregarTareas> {
               
               const SizedBox(height: 20,),
 
+              _buildSectionContainer(
+                items: [
+                  _buildSettingItem(
+                    icon: Icons.date_range_outlined,
+                    title: 'Fecha: ',
+                    trailing: Icon(Icons.arrow_forward_ios, color: Colors.grey[600], size: 18),
+                    ),
+
+                  _buildSettingItem(
+                    icon: Icons.hourglass_bottom_outlined,
+                    title: 'Hora: ',
+                    trailing: Icon(Icons.arrow_forward_ios, color: Colors.grey[600], size: 18),
+                  ),
+
+                  _buildSettingItem(
+                    icon: Icons.heart_broken_outlined,
+                    title: 'Favoritos: ',
+                    trailing: Switch(
+                      value: true,
+                      onChanged: (value) {
+                        
+                      },
+                      activeColor: Colors.black,
+                    ),
+                  ),
+                  
+                ]
+              ),
             ],
           ),
         ),
@@ -46,6 +74,44 @@ class _AgregarTareasState extends State<AgregarTareas> {
   }
 
   Widget _buildSectionContainer({required List<Widget> items}){
-    return Container();
+    return Container(
+      padding: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: const [
+          BoxShadow(
+            color: Color.fromARGB(26, 190, 189, 189),
+            spreadRadius: 2,
+            blurRadius: 10,
+            offset: Offset(0, 5),
+          )
+        ] 
+        ),
+        child: Column(children: items),
+    );
+  }
+
+  Widget _buildSettingItem({required IconData icon, required String title, required Widget trailing}){
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Icon(icon, color: Colors.grey,),
+              const SizedBox(width: 15,),
+              Text(
+                title, 
+                style: const TextStyle(fontSize: 16, color: Colors.black),
+
+              )
+            ],
+          ),
+          trailing,
+        ],
+      ),
+      );
   }
 }
