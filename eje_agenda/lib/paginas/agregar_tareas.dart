93cf05bd3421ed2b9f1e3a8b3d0a73091f8009dd
eventuraa15 <141580602+Eduar1505@users.noreply.github.com>
2024-11-30 11:45:43 -0600

    agregartareas pagina

--- eje_agenda/lib/paginas/agregar_tareas.dart
+++ eje_agenda/lib/paginas/agregar_tareas.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
 
-class AgregarTareas extends StatelessWidget {
+class AgregarTareas extends StatefulWidget {
   const AgregarTareas({super.key});
 
   @override
+  State<AgregarTareas> createState() => _AgregarTareasState();
+}
+
+class _AgregarTareasState extends State<AgregarTareas> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    final ThemeData theme = Theme.of(context);
+    return Scaffold(
+      
+      // Color de fondo 
+      backgroundColor: Colors.black,
+
+      appBar: AppBar(title: const Text('Agregar Tarea' ),centerTitle: true, /*backgroundColor: Colors.black*/ ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(borderRadius: BorderRadius.zero), 
+                  label: Text('Nombre'),
+                  
+                  ),
+              ),
+              const SizedBox(height: 20,),
+
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(borderRadius: BorderRadius.zero), 
+                  label: Text('Descripcion'),
+                  ),
+
+              )
+            ],
+
+          ),
+        ),
+    );
   }
 }
