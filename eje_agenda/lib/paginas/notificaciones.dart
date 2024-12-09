9ac3229c5ce187728cf8009ff599b79de12b1d64
01molina <jdmolinar@unah.hn>
2024-12-08 22:02:40 -0600

    notificacion

---
+++ eje_agenda/lib/paginas/notificaciones.dart
@@ -0,0 +1,13 @@
+import 'package: flutter_local_notifications/flutter_local_notifications.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+Future<void> initNotifications() async {
+  const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('app_icon');
+
+  const InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+}
