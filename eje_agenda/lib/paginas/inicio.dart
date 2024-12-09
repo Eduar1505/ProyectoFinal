b18076ed1f6f6148ed1777236bf935c25ff8c821
EmersonGalindo <emerson.galindo@unah.hn>
2024-12-08 20:03:46 -0600

    pantalla inicio

--- eje_agenda/lib/paginas/inicio.dart
+++ eje_agenda/lib/paginas/inicio.dart
@@ -1,36 +1,106 @@
+import 'package:flutter/material.dart';
 import 'package:eje_agenda/componentes.dart/botones.dart';
 import 'package:eje_agenda/paginas/home_page.dart';
-import 'package:eje_agenda/paginas/inicio2.dart';
-import 'package:flutter/material.dart';
 
-class StartPage extends StatelessWidget {
-  const StartPage({super.key});
+
+class inicio extends StatefulWidget {
+  const inicio({super.key});
+
+  @override
+  State<inicio> createState() => _inicioState();
+}
+
+class _inicioState extends State<inicio> {
+  late Color myColor;
+  late Size mediaSize;
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      backgroundColor: Colors.indigo[300],
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(Icons.event_note, size: 100, color: Color.fromARGB(255, 55, 111, 157),),
-            const SizedBox(height: 20,),
-            const Text('EJE AGENDA', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),),
-            const SizedBox(height: 50,),
-            
-            MyButton(
-              onTap: (context) {
-              Navigator.push(
-               context,
-                MaterialPageRoute(builder: (context) => const NamePage()),
+    myColor = Theme.of(context).primaryColor;
+    mediaSize = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [Colors.indigo, Colors.black],
+        ),
+      ),
+      
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(children: [
+          Positioned(top: 80, child: _buildTop()),
+          Positioned(bottom: 0, child: _buildBottom()),
+        ]),
+      ),
+    );
+  }
+
+  //icono y texto
+  Widget _buildTop() {
+    return SizedBox(
+      width: mediaSize.width,
+      child: const Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            Icons.book,
+            size: 100,
+            color: Colors.white,
+          ),
+          Text(
+            "EJE Agenda",
+            style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+                letterSpacing: 2),
+          )
+        ],
+      ),
+    );
+  }
+
+  //
+  Widget _buildBottom() {
+    return SizedBox(
+      width: mediaSize.width,
+      child: Card(
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        )),
+        child: Padding(
+          padding: const EdgeInsets.all(35.0),
+          child: Container(child: _buildForm()),
+        ),
+      ),
+    );
+  }
+
+  //
+  Widget _buildForm() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        MyButton(
+          onTap: (context) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const HomePage()),
             );
           },
         ),
-
-          ]
+        Image.asset(
+          'assets/images/unah_logo.png',
+          width: 270,
+          height: 200,
         ),
-      ),
+      ],
     );
   }
 }
