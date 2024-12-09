d6ff57ae489a5f84a346632c1e06405e27c92c26
01molina <jdmolinar@unah.hn>
2024-12-08 18:53:35 -0600

    BaseDatos2

--- eje_agenda/lib/database/db_helper.dart
+++ eje_agenda/lib/database/db_helper.dart
@@ -13,5 +13,19 @@ class DatabaseHelper {
     return _instance;
   }
 
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    String path = join(await getDatabasesPath(), 'tareas.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
 
 }
