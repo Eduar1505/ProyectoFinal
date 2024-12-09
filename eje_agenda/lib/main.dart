3e1d5a9c2b8a3924bb7b78ced22f041346a087d1
EmersonGalindo <emerson.galindo@unah.hn>
2024-12-08 21:25:47 -0600

    Update main.dart

--- eje_agenda/lib/main.dart
+++ eje_agenda/lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        '/': (context) => const StartPage(),
+        '/': (context) => const inicio(),
         '/start': (context) => const SplashScreen(),
         '/start2': (context) => const HomePage(),
       },
